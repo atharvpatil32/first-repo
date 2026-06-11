24cd6189d7858fc02479984cf50268df02c59881
atharvpatil32 <atharvpatil5096@gmail.com>
2026-06-11 12:58:55 +0530

    day 4

---
+++ Day 4.py
@@ -0,0 +1,75 @@
+# a = 10
+# b = 20
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a//b)
+# print(a/b)
+# print(a**b)
+# print(a%b)
+
+
+# a = float(input("Enter 1st number: "))
+# b = float(input("Enter 2nd number to"))
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a//b)
+# print(a/b)
+# print(a**b)
+# print(a%b)
+
+
+# a = 15
+# b = 20
+# print(a==b)
+# print(a>=b)
+# print(a<=b)
+# print(a!=b)
+# print()
+
+# age=int(input("What is your age?"))
+# if age>=18:
+#     print("You are elligible to vote")
+# else:
+#     print("You are not elligible to vote")
+
+
+# a = 40
+# b = 25
+# print(a>22 and b>23)
+# print(a<23 and b<22)
+# print(not(a>22))
+# print(not(a<23))
+# print(a>21 or b<22)
+# print(a<21 or b>22)
+
+#
+# a = input("Enter a student name:")
+# print(a)
+# marks = int(input("Enter student marks"))
+# if marks > 45:
+#     print("Paas...")
+# if marks > 75:
+#     print("Destinction marks....")
+# else:
+#     print("Fail...")
+
+#
+# a = 10
+# b = 7
+# print(a>6 and b<2)
+# print(a>5 and b<7)
+# print(a>4 or b<3)
+# print(a>3 or b<5)
+# print(not(a>22))
+# print(not(a<23))
+
+
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even number ")
+else:
+    print("Odd number ")
